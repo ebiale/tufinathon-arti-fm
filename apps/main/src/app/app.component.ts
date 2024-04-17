c9cdb22273d7b0547cdea5310e3e474a2ad798a4
elena.bialyk <elena.bialyk@tufin.com>
2024-04-17 11:37:35 +0300

    logo added, css fixes

--- apps/main/src/app/app.component.ts
+++ apps/main/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, inject } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { CodeEditorComponent } from './components/code-editor/code-editor.component';
-import { JsonPipe } from '@angular/common';
-import { LanguageSelectorComponent } from './components/language-selector/language-selector.component';
+import {Component, inject} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import {CodeEditorComponent} from './components/code-editor/code-editor.component';
+import {JsonPipe, NgOptimizedImage} from '@angular/common';
+import {LanguageSelectorComponent} from './components/language-selector/language-selector.component';
 import {ResponseContentComponent} from './components/content/response-content.component';
-import { ArtiControlsService } from './services/arti-controls.service';
+import {ArtiControlsService} from './services/arti-controls.service';
 
 @Component({
   standalone: true,
@@ -13,17 +13,22 @@ import { ArtiControlsService } from './services/arti-controls.service';
     ReactiveFormsModule,
     JsonPipe,
     LanguageSelectorComponent,
-    ResponseContentComponent
+    ResponseContentComponent,
+    NgOptimizedImage
   ],
   selector: 'taf-root',
   template: `
     <div class="top">
+      <img class="logo" src="./../assets/img.png" >
       <span class="title">Hey There Tufiner!</span>
-      <span class="sub-title">Drop sample code here and let's do some magic:</span>
+
     </div>
     <div class="content code-editor-wrapper">
-      <taf-language-selector class="language-selector" [formControl]="languageCtrl" />
-      <taf-code-editor  [formControl]="codeCtrl" [language]="languageCtrl.value" />
+      <div class="header">
+        <span class="sub-title">Drop sample code here and let's do some magic:</span>
+        <taf-language-selector class="language-selector" [formControl]="languageCtrl"/>
+      </div>
+      <taf-code-editor [formControl]="codeCtrl" [language]="languageCtrl.value"/>
     </div>
 
     <taf-response-content class="content response-content"/>
